e15ae78137b6b474eb76ca74e665a7c495216349
Jona Hamm <jonahamm2004@gmail.com>
2026-02-06 10:49:34 +0100

    Add Input event to clear error

--- src/app.js
+++ src/app.js
@@ -8,26 +8,35 @@ const subscriptionEmail = document.querySelector(".subscription-email");
 
 const dismissBtn = document.querySelector(".dismiss-btn");
 
+// Custom submission logic (prevents default behavior)
 const handleSubmit = (e) => {
   e.preventDefault();
 
   validateInputs();
 };
 
+// Sets the error state and error message in the form
 const setError = (message) => {
   errorDisplay.innerText = message;
   emailInput.classList.add("error");
 };
 
-const subscribeNewsletter = (email) => {
+// Remove the error state and error message
+const clearError = () => {
   errorDisplay.innerText = "";
   emailInput.classList.remove("error");
+}
 
+// Sets the email in the success message and reveals it
+const subscribeNewsletter = (email) => {
   subscriptionEmail.innerText = email;
+  emailInput.value = "";
+
   newsletterSignup.style.display = "none";
   newsletterSuccess.style.display = "flex";
 };
 
+// Checks the form for a valid email address
 const validateInputs = () => {
   const emailValue = emailInput.value.trim();
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -43,10 +52,12 @@ const validateInputs = () => {
   subscribeNewsletter(emailValue);
 };
 
+// Hides the success message and reveals the sign-up form
 const hideMessage = (e) => {
   newsletterSignup.style.display = "flex";
   newsletterSuccess.style.display = "none";
 };
 
+emailInput.addEventListener("input", clearError);
 signUpForm.addEventListener("submit", handleSubmit);
 dismissBtn.addEventListener("click", hideMessage);
